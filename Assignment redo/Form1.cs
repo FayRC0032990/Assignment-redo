3b7399ee425ca54daafa0c6d91483d78f8a82d6c
rc0032990 <rc0032990@CRO-24-05>
2019-06-18 09:53:11 +0100

    calculations done for parallel and series

--- Assignment redo/Form1.cs
+++ Assignment redo/Form1.cs
@@ -25,7 +25,10 @@ namespace Assignment_redo
             Double R4 = Convert.ToDouble(textBox4.Text);
             Double R5 = Convert.ToDouble(textBox5.Text);
 
-            
+            Double RS = R1 + R2 + R3 + R4 + R5;
+            Double RP = 1 / (R1 + 1 / R2 + 1 / R3 + 1 / R4 + 1 / R5);
+            label6.Text = " Answer Series" + RS;
+            label7.Text = " Answer Parallel" + RP;
         }
     }
 }
